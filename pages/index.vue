caff0c8a4ef295fa9dd13d1326cbad3f0d27c460
Ibwedagama <wardana.wedagama@gmail.com>
2023-10-09 16:20:37 +0800

    add vscode extention recommendation

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,5 @@
 <template>
   <div class="h-full p-8">
-    <h1
-      class="absolute h-full w-full bg-green-200 font-mono text-3xl shadow-md"
-    >
-      Jds Builder
-    </h1>
+    <h1 class="text-3xl">Jds Builder</h1>
   </div>
 </template>
