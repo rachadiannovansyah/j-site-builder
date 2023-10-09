145fe2fc355abc65aeba105d7400db149fdd44e3
Ibwedagama <wardana.wedagama@gmail.com>
2023-10-09 16:02:30 +0800

    setup eslint + prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="h-full p-8">
-    <h1 class="text-3xl font-bold">
-      J-Site Builder
+    <h1
+      class="absolute h-full w-full bg-green-200 font-mono text-3xl shadow-md"
+    >
+      Jds Builder
     </h1>
   </div>
 </template>
+
+<script setup lang="ts">
+  const myName = 'Weda'
+
+  console.log(myName)
+</script>
+
+<style scoped>
+  .footer {
+    text-shadow: none;
+  }
+</style>
