3d7d21ebf833fccf755d2558c680a5f1cca05b8a
Ibwedagama <wardana.wedagama@gmail.com>
2023-10-09 16:25:37 +0800

    add prettier plugin on eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,21 +5,25 @@ module.exports = {
     node: true,
   },
   extends: [
-    "plugin:@typescript-eslint/recommended",
-    "plugin:nuxt/recommended",
-    "plugin:vue/vue3-recommended",
-    "prettier",
+    'plugin:@typescript-eslint/recommended',
+    'plugin:nuxt/recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+    'prettier',
   ],
   parserOptions: {
-    ecmaVersion: "2020",
-    parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    ecmaVersion: '2020',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module',
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ['@typescript-eslint'],
   rules: {
-    "vue/script-setup-no-uses-vars": "off",
-    "vue/multi-word-component-names": ["error", {
-      "ignores": ['index']
-    }]
+    'vue/script-setup-no-uses-vars': 'off',
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['index'],
+      },
+    ],
   },
-};
+}
