74e3e2491f4bca9516d64a2974fd4807968de10d
Ibwedagama <wardana.wedagama@gmail.com>
2023-10-19 13:39:01 +0800

    feat: create base modal component

---
+++ components/ModalFooter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    :class="{
+      'flex items-center gap-4 bg-gray-50 px-6 py-4': true,
+      'justify-center': props.position === 'center',
+      'justify-start': props.position === 'left',
+      'justify-end': props.position === 'right',
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+  type PositionTypes = 'center' | 'left' | 'right'
+
+  const props = defineProps({
+    position: {
+      type: String as () => PositionTypes,
+      default: 'center',
+    },
+  })
+</script>
---
+++ components/ModalTitle.vue
@@ -0,0 +1,12 @@
+<template>
+  <DialogTitle
+    as="h3"
+    class="pl-6 pr-16 pt-4 font-roboto text-xl font-medium leading-8 text-green-800"
+  >
+    <slot />
+  </DialogTitle>
+</template>
+
+<script setup lang="ts">
+  import { DialogTitle } from '@headlessui/vue'
+</script>
