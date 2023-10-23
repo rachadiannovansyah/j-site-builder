4e981e8ebd57354c657aad01da0b93a9897fb666
Ibwedagama <wardana.wedagama@gmail.com>
2023-10-23 16:14:46 +0800

    feat (auth): add verify page

---
+++ pages/verify/[token].vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { setCookie } from '@/common/helpers/cookie.ts'
+  import { onMounted } from 'vue'
+
+  const site = useSiteStore()
+  const loading = useLoadingStore()
+  const route = useRoute()
+
+  onMounted(() => {
+    loading.setLoading(true)
+
+    const token = route.params.token
+    const siteId = route.query.id
+
+    if (token && siteId) {
+      setTimeout(() => {
+        site.setSideId(siteId)
+        setCookie('token', token)
+
+        loading.setLoading(false)
+        navigateTo('/')
+      }, 1500)
+    }
+  })
+</script>
