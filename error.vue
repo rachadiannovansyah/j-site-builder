c3ad27c25ef652fa6e3191044d8d66d46e0f7e6c
Ida Bagus Wedagama <33661143+Ibwedagama@users.noreply.github.com>
2023-10-24 11:05:13 +0800

    feat: auth middleware (#13)
    * feat (auth): add global auth middleware
    
    * feat (error): add unauthorized error page
    
    * chore (error): add todo comment

--- error.vue
+++ error.vue
@@ -2,6 +2,12 @@
   <NuxtLayout v-if="error?.statusCode === 404">
     <UnderConstruction />
   </NuxtLayout>
+
+  <NuxtLayout v-else-if="error?.statusCode === 401" name="public">
+    <!-- TODO: improve design of unauthorized error  -->
+    <h1 class="text-3xl font-bold">{{ error }}</h1>
+  </NuxtLayout>
+
   <div v-else class="p-8">
     <h1 class="text-3xl font-bold">{{ error }}</h1>
     <button @click="handleError">Clear errors</button>
---
+++ layouts/public.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex h-screen w-full items-center justify-center overflow-y-auto">
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
