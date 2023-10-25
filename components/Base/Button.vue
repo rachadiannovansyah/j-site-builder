60545c5199a48858cd69b2d257fdd035192a785e
Ibwedagama <wardana.wedagama@gmail.com>
2023-10-25 16:19:53 +0800

    feat (button): add base button component

---
+++ components/Base/Button.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    v-bind="$attrs"
+    :class="{
+      'flex h-[38px] min-w-fit rounded-lg border border-transparent font-lato text-sm font-bold transition-colors ease-in': true,
+      'bg-green-700 px-4 py-[10px] text-white hover:bg-green-500':
+        props.variant === 'primary',
+      'border-green-700 bg-transparent px-4 py-[10px] text-green-700 hover:bg-green-50':
+        props.variant === 'secondary',
+      'bg-transparent px-4 py-[10px] text-green-700 hover:bg-green-50':
+        props.variant === 'tertiary',
+      'bg-transparent text-green-700 hover:bg-green-50':
+        props.variant === 'tertiary-paddingless',
+    }"
+    @click="$emit('click')"
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+  type IButtonVariant =
+    | 'primary'
+    | 'secondary'
+    | 'tertiary'
+    | 'tertiary-paddingless'
+
+  const props = defineProps({
+    variant: {
+      type: String as PropType<IButtonVariant>,
+      default: 'primary',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  })
+
+  defineEmits(['click'])
+</script>
