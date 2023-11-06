bc3dc0566ef77805403c80dddb50028b6464e3c6
Ibwedagama <wardana.wedagama@gmail.com>
2023-11-06 17:04:21 +0800

    feat: add user data to auth store

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,9 +1,21 @@
 import { removeAllCookies } from '@/common/helpers/cookie'
+import { IUser } from '~/types/stores/auth'
 
 export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    user: null as null | IUser,
+  }),
+  getters: {
+    userName(state) {
+      return state.user?.role.name
+    },
+  },
   actions: {
     logout() {
       removeAllCookies()
+    },
+    setUser(user: IUser) {
+      this.user = user
     },
   },
 })
