37e2e6c93f9c0ff09b385c970f72e7352d8cd49a
Yoslie <yoslie.tan@gmail.com>
2023-11-08 17:28:21 +0700

    style: add gap aside component

--- pages/halaman/buat.vue
+++ pages/halaman/buat.vue
@@ -2,7 +2,7 @@
   <div class="flex h-screen w-full flex-col overflow-hidden bg-[#F3F4F8]">
     <PageHeaderCreate />
     <div
-      class="mb-4 flex h-full w-full justify-between overflow-y-auto px-1 py-4"
+      class="mb-4 flex h-full w-full justify-between gap-4 overflow-y-auto px-1 py-4"
     >
       <PageContentCreate :data="data.sections" />
       <PageAsideCreate />
