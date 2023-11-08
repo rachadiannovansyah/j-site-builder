a7fcb89a57b6655866e181b5e804fde863b1419e
Yoslie <yoslie.tan@gmail.com>
2023-11-08 11:03:04 +0700

    update: set action on preview template

--- pages/halaman/semua.vue
+++ pages/halaman/semua.vue
@@ -250,7 +250,9 @@
                 />
               </div>
             </div>
-            <button
+            <a
+              :href="template.preview"
+              target="_blank"
               class="absolute right-2 top-2 rounded-md border border-gray-500"
             >
               <NuxtIcon
@@ -258,7 +260,7 @@
                 aria-hidden="true"
                 class="text-[20px] text-gray-500"
               />
-            </button>
+            </a>
             <div class="flex justify-between">
               <div class="flex flex-col">
                 <RadioGroupLabel
