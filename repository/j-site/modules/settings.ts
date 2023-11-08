887c1abdcd117fe59fd3f08f3402692cbd4ac4ae
Ibwedagama <wardana.wedagama@gmail.com>
2023-11-08 15:06:10 +0800

    feat (repository): add settings repository

---
+++ repository/j-site/modules/settings.ts
@@ -0,0 +1,25 @@
+import { AsyncDataOptions } from '#app'
+import { FetchOptions } from 'ofetch'
+
+import FetchFactory from '../../factory'
+import { ISettingsResponse } from '../types/settings'
+
+class TemplateModules extends FetchFactory {
+  private RESOURCE = '/v1/settings'
+
+  async getSettings(
+    fetchOptions?: FetchOptions<'json'>,
+    options?: AsyncDataOptions<ISettingsResponse>,
+  ) {
+    return useAsyncData(() => {
+      return this.call<ISettingsResponse>(
+        'GET',
+        `${this.RESOURCE}`,
+        undefined, // body
+        fetchOptions,
+      )
+    }, options)
+  }
+}
+
+export default TemplateModules
---
+++ repository/j-site/types/settings.ts
@@ -0,0 +1,31 @@
+export interface ISiteSetting {
+  _id: string
+  created_by: string
+  name: string
+  favicon: string
+  color_palette?: unknown
+  subdomain: string
+  organization: string
+  is_active: boolean
+  logo: string
+  navigation?: unknown
+  footer?: unknown
+  created_at: string
+  updated_at: string
+  domain: string
+  id: string
+}
+
+export interface IMetaData {
+  page: number
+  last_page: number
+  limit: number
+  from: number
+  to: number
+  total: number
+}
+
+export interface ISettingsResponse {
+  data: ISiteSetting[]
+  meta: IMetaData
+}
