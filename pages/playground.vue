f4df7ec51b4d00d75e38e66565512dd1abcb5c61
Ibwedagama <wardana.wedagama@gmail.com>
2023-11-14 15:55:03 +0800

    feat: add link button example

--- pages/playground.vue
+++ pages/playground.vue
@@ -1,12 +1,22 @@
 <template>
   <main class="mx-auto max-w-xl py-20">
     <h1>Buttons</h1>
-    <div class="flex gap-x-8 gap-y-8 py-4 flex-wrap">
+    <div class="flex flex-wrap gap-x-8 gap-y-8 py-4">
       <UButton>Default</UButton>
       <UButton variant="outline">Default</UButton>
       <UButton color="red">Default</UButton>
       <UButton color="red" variant="outline">Default</UButton>
       <UButton block>Default</UButton>
+    </div>
+    <h1>Link Button</h1>
+    <div class="flex flex-wrap gap-x-8 gap-y-8 py-4">
+      <UButton to="/">Home</UButton>
+      <UButton to="https://google.com" target="_blank" variant="outline">
+        Google
+      </UButton>
+      <UButton color="red" variant="outline" @click="$router.back()">
+        Kembali
+      </UButton>
     </div>
   </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-import type { Config } from 'tailwindcss'
+/** @type {import('tailwindcss').Config} */
 
-export default <Partial<Config>>{
+export default {
   theme: {
     extend: {
       fontFamily: {
