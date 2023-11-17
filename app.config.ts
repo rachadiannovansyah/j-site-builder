bc32d90102e99121019a0bcf795dd458818895b5
Ibwedagama <wardana.wedagama@gmail.com>
2023-11-17 13:23:01 +0800

    fix (app.config.ts): move modal config; remove moda.ts

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,6 @@
 import buttonStyle from '~/assets/nuxt-ui/button'
 import inputStyle from '~/assets/nuxt-ui/input'
 import tableStyle from '~/assets/nuxt-ui/table'
-import modalStyle from '~/assets/nuxt-ui/modal'
 
 export default defineAppConfig({
   ui: {
@@ -10,6 +9,16 @@ export default defineAppConfig({
     button: buttonStyle,
     input: inputStyle,
     table: tableStyle,
-    modal: modalStyle,
+    modal: {
+      overlay: {
+        background: 'bg-black/50',
+      },
+      rounded: 'rounded-xl',
+      width: 'sm:max-w-lg',
+      transition: {
+        enterFrom: 'sm:translate-y-20 sm:scale-100',
+        leaveTo: 'sm:translate-y-20 sm:scale-100',
+      },
+    },
   },
 })
