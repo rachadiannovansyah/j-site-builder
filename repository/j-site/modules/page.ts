afda150a620e3d350ef9bff66a180e0147e79928
Ibwedagama <wardana.wedagama@gmail.com>
2023-11-17 12:03:46 +0800

    feat (page): add get pages on page repository

--- repository/j-site/modules/page.ts
+++ repository/j-site/modules/page.ts
@@ -1,10 +1,26 @@
 import { AsyncDataOptions } from '#app'
+import { FetchOptions } from 'ofetch'
 
 import FetchFactory from '../../factory'
-import { IPageResponse, IPageData } from '../types/page'
+import { IPageResponse, IPageData, IPagesResponse } from '../types/page'
 
 class PageModules extends FetchFactory {
   private RESOURCE = '/v1/pages'
+
+  async getPages(
+    id: string,
+    fetchOptions?: FetchOptions<'json'>,
+    options?: AsyncDataOptions<IPagesResponse>,
+  ) {
+    return useAsyncData(() => {
+      return this.call<IPagesResponse>(
+        'GET',
+        `${this.RESOURCE}/${id}`,
+        undefined, // body
+        fetchOptions,
+      )
+    }, options)
+  }
 
   async storePage(
     id: string,
--- repository/j-site/types/page.ts
+++ repository/j-site/types/page.ts
@@ -9,3 +9,28 @@ export interface IPageResponse {
   status?: object
   error?: object
 }
+export interface IPageDataExtended {
+  _id: string
+  created_by: string
+  title: string
+  sections: unknown[]
+  is_active: boolean
+  status: string
+  created_at: string
+  updated_at: string
+  slug: string
+}
+
+export interface IMetaData {
+  page: number
+  last_page: number
+  limit: number
+  from: number
+  to: number
+  total: number
+}
+
+export interface IPagesResponse {
+  data?: IPageDataExtended[]
+  meta?: IMetaData
+}
