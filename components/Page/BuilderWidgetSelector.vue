eced1ae41f5ad0e0527f8ecd3a96ecb127e2c2ce
Ibwedagama <wardana.wedagama@gmail.com>
2023-11-20 16:19:32 +0800

    feat (builder): add BuilderWidgetSelector component

---
+++ components/Page/BuilderWidgetSelector.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <!-- @todo: add widget selection menu and grid selector for specific widget -->
+    <UPopover class="mb-3">
+      <UButton color="white" trailing-icon="i-heroicons-chevron-down-20-solid">
+        {{ props.widget }}
+      </UButton>
+      <template #panel>
+        <div class="p-4">
+          <div class="h-[400px] w-[600px] bg-gray-200" />
+        </div>
+      </template>
+    </UPopover>
+    <p class="mb-8 text-center font-lato text-sm leading-6 text-gray-500">
+      Setup element sesuai dengan kebutuhan kamu
+    </p>
+    <div class="max-w-[200px]">
+      <UButton @click="toggleConfig">
+        <template #leading>
+          <NuxtIcon
+            name="common/cog-wheel"
+            class="text-lg"
+            aria-hidden="true"
+          />
+        </template>
+        Setup Konten
+      </UButton>
+    </div>
+  </div>
+
+  <component :is="widgetConfig" :open="isConfigOpen" @close="toggleConfig" />
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    widget: {
+      type: String,
+      default: '',
+    },
+    widgetIndex: {
+      type: Number,
+      default: null,
+    },
+    sectionIndex: {
+      type: Number,
+      default: null,
+    },
+  })
+
+  // Widget Dynamic import
+  const WidgetSlideshow = resolveComponent('WidgetSlideshow')
+  // add another widget here...
+
+  const isConfigOpen = ref(false)
+
+  function toggleConfig() {
+    isConfigOpen.value = !isConfigOpen.value
+  }
+
+  const widgetConfig = computed(() => {
+    switch (props.widget) {
+      case 'Slideshow':
+        return WidgetSlideshow
+      // add another case here
+      default:
+        break
+    }
+  })
+</script>
