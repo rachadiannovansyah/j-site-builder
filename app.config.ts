e3b3296934a10592cf4b096c0f968bf3204ba1b9
Ibwedagama <wardana.wedagama@gmail.com>
2023-11-22 12:30:57 +0800

    refactor (app.config): improve table override style

--- app.config.ts
+++ app.config.ts
@@ -54,9 +54,14 @@ export default defineAppConfig({
       },
     },
     table: {
+      default: {
+        sortButton: {
+          color: 'primary',
+        },
+      },
       wrapper: 'border border-gray-300 rounded-lg',
       divide: 'divide-y divide-gray-300',
-      thead: 'bg-primary-600',
+      thead: 'bg-green-600',
       tbody: 'divide-y divide-gray-300',
       tr: {
         base: 'even:bg-gray-50',
@@ -64,19 +69,13 @@ export default defineAppConfig({
       th: {
         padding: 'px-3 py-3',
         color: 'text-white',
-        font: 'font-semibold font-roboto',
+        font: 'font-bold font-roboto text-sm leading-6',
       },
       td: {
         base: 'whitespace-normal',
         padding: 'px-3 py-3',
         color: 'text-gray-800',
-        font: 'font-lato',
-      },
-      loadingState: {
-        wrapper:
-          'flex flex-col items-center justify-center flex-1 px-6 py-14 sm:px-14',
-        label: 'text-sm text-center text-gray-900 dark:text-white',
-        icon: 'w-6 h-6 mx-auto text-gray-400 dark:text-gray-500 mb-4 animate-spin',
+        font: 'font-lato text-sm leading-6',
       },
     },
     modal: {
