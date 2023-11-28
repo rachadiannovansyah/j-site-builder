d4488cd68523f5f2e4749f7b906e18b3caa086e3
Yoslie <yoslie.tan@gmail.com>
2023-11-28 14:46:58 +0700

    update: change content preview

--- components/Widget/PostCrawler.vue
+++ components/Widget/PostCrawler.vue
@@ -7,10 +7,14 @@
     <UCard
       :ui="{
         body: {
-          padding: 'sm:py-4 sm:px-6',
+          padding: 'sm:py-2 sm:px-6',
         },
         header: {
           padding: 'sm:py-4 sm:px-6',
+        },
+        footer: {
+          background: 'bg-gray-50',
+          padding: 'sm:py-3 sm:px-6',
         },
       }"
     >
@@ -110,10 +114,21 @@
         </div>
 
         <div
-          class="mb-4 flex max-h-[311px] w-full min-w-0 items-center justify-center overflow-y-auto rounded-[14px] border border-gray-300 bg-[#F9F9F9] p-2.5 px-6"
+          :class="{
+            'flex max-h-[311px] w-full min-w-0 items-center justify-center rounded-[14px] border border-gray-400 bg-[#F9F9F9] p-2.5 px-6': true,
+            'bg-white': selectedPostFormat,
+          }"
         >
           <template v-if="selectedPostFormat">
-            {{ selectedPostFormat }}
+            <div class="flex max-h-[294px] w-full overflow-y-auto pt-4">
+              <NuxtImg
+                :src="selectedPostPreview"
+                width="220"
+                height="220"
+                class="h-full w-full"
+                alt="Ilustrasi tidak ada format post terpilih"
+              />
+            </div>
           </template>
 
           <template v-else>
@@ -130,10 +145,23 @@
               <h1 class="font-roboto text-[21px] font-medium text-gray-900">
                 Pilih Format Tampilan terlebih dahulu
               </h1>
+              <p class="font-lato text-sm font-normal text-[#616161]">
+                Untuk preview tampilan silakan pilih format tampilan terlebih
+                dahulu
+              </p>
             </div>
           </template>
         </div>
       </section>
+
+      <template #footer>
+        <section class="flex justify-between">
+          <UButton variant="ghost" color="gray" @click="$emit('close')">
+            Batal
+          </UButton>
+          <UButton> Simpan </UButton>
+        </section>
+      </template>
     </UCard>
   </UModal>
 </template>
@@ -160,21 +188,25 @@
       label: 'Thumbnail View',
       value: 'thumbnail',
       icon: '/images/post/thumbnail-view.svg',
+      preview: '/images/post/thumbnail-preview.svg',
     },
     {
       label: 'List View',
       value: 'list',
       icon: '/images/post/list-view.svg',
+      preview: '/images/post/list-preview.svg',
     },
     {
       label: 'Bar View',
       value: 'bar',
       icon: '/images/post/bar-view.svg',
+      preview: '/images/post/bar-preview.svg',
     },
   ]
 
   const selectedPostType = ref('')
   const selectedPostFormat = ref('')
+  const selectedPostPreview = ref('')
 
   const props = defineProps({
     open: {
@@ -184,4 +216,21 @@
   })
 
   defineEmits(['close'])
+
+  watch(
+    selectedPostFormat,
+    (value) => {
+      if (value) {
+        const findPost = POST_FORMAT_DISPLAY.find(
+          ({ value }) => value === selectedPostFormat.value,
+        )
+        setPostPreview(findPost?.preview || '')
+      }
+    },
+    { immediate: true },
+  )
+
+  function setPostPreview(value: string) {
+    selectedPostPreview.value = value
+  }
 </script>
