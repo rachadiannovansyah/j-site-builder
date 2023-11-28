e5aae8376e4f71629c6fdd96352b5fa56b6465ff
Ibwedagama <wardana.wedagama@gmail.com>
2023-11-28 16:53:22 +0800

    fix (validation): fix error message

--- common/helpers/validation.ts
+++ common/helpers/validation.ts
@@ -45,7 +45,8 @@ export async function validateImage(
         return fileSize <= MAX_FILE_SIZE
       },
       {
-        message: `Ukuran gambar boleh melebihi ${getFileSize(MAX_FILE_SIZE)}!`,
+        message: `
+        Ukuran gambar tidak boleh melebihi ${getFileSize(MAX_FILE_SIZE)}!`,
       },
     )
     .refine(
