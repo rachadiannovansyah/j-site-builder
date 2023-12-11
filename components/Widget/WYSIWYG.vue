b5cc8af81180574e314d12bd35c38a898348af25
Ibwedagama <wardana.wedagama@gmail.com>
2023-12-11 10:11:17 +0800

    feat (wysiwyg): handle image upload

--- components/Widget/WYSIWYG.vue
+++ components/Widget/WYSIWYG.vue
@@ -49,7 +49,12 @@
 
 <script setup lang="ts">
   import Editor from '@tinymce/tinymce-vue'
+  import { validateImage } from '~/common/helpers/validation'
+  import { z } from 'zod'
+
   const config = useRuntimeConfig()
+  const siteStore = useSiteStore()
+  const { $jSiteApi } = useNuxtApp()
 
   const props = defineProps({
     open: {
@@ -68,13 +73,16 @@
       language: 'id',
       skin_url: '/tinymce/skins',
       menubar: 'false',
-      plugins: 'lists link image media',
+      plugins: 'preview fullscreen lists link image media',
       toolbar: `
-      undo redo | blocks | bold italic bullist numlist blockquote | 
+      undo redo | blocks | bold italic bullist numlist blockquote |
       alignleft aligncenter alignright alignjustify |
-      link strikethrough indent outdent image media`,
+      link strikethrough indent outdent image media |
+      preview fullscreen`,
+      images_upload_handler: onImageUpload,
       toolbar_mode: 'sliding',
       image_caption: true,
+      paste_block_drop: true,
       invalid_elements: 'div',
     },
   })
@@ -86,6 +94,52 @@
   function editorSetup(editor: any) {
     editor.on('init', () => {
       isEditorLoading.value = false
+    })
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function onImageUpload(blobInfo: any) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        await validateImage(blobInfo.blob(), {
+          maxSize: 2097152, // 2MB
+          maxWidth: 1024, // 1024 pixel
+          maxHeight: 576, // 576 pixel
+        })
+
+        const formData = new FormData()
+
+        formData.append('file', blobInfo.blob())
+        formData.append('caption', 'wysiwyg')
+        formData.append('category', 'wysiwyg')
+        formData.append('setting_id', siteStore.siteId ?? '')
+
+        const { data: uploadResponse, status } =
+          await $jSiteApi.media.uploadMedia(formData, undefined, {
+            server: false,
+          })
+
+        if (status.value === 'success') {
+          const fileURL = toRaw(uploadResponse.value?.data.file.uri)
+          resolve(fileURL)
+        }
+
+        if (status.value === 'error') {
+          reject({
+            message: `Mohon maaf, terjadi kesalahan saat proses upload gambar.
+            Silakan coba beberapa saat lagi.`,
+            remove: true,
+          })
+        }
+      } catch (error) {
+        // Validation error throw by Zod
+        if (error instanceof z.ZodError) {
+          reject({
+            message: error.issues[0].message,
+            remove: true,
+          })
+        }
+      }
     })
   }
 
