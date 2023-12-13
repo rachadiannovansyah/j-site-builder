ff8927e22c2fbe2d86cdd8215a290743a3553fa0
Yoslie <yoslie.tan@gmail.com>
2023-12-13 12:48:39 +0700

    feat: slicing layout list post

--- pages/posting/semua.vue
+++ pages/posting/semua.vue
@@ -8,7 +8,10 @@
         <FilterBar />
       </div>
     </div>
-    <div class="flex h-full w-full flex-col items-center justify-center">
+    <div
+      v-if="data.length === 0"
+      class="flex h-full w-full flex-col items-center justify-center"
+    >
       <NoData
         title="Kamu belum memiliki Post !"
         description="Tenang saja, kita siap membantu kamu memulainya dengan informasi konten
@@ -26,6 +29,12 @@
         </UButton>
       </NoData>
     </div>
+
+    <div v-else class="flow-root">
+      <ul role="list" class="flex flex-col gap-[10px]">
+        <PostList :data="data" />
+      </ul>
+    </div>
   </section>
 </template>
 
@@ -33,4 +42,56 @@
   definePageMeta({
     title: 'Posting',
   })
+
+  // TODO: replace dummy data with service API
+  const data = [
+    {
+      title:
+        'The Importance of Public Services in Improving Quality of Life Life Life Life Life Life',
+      updated_at: '06/02/2023',
+      category: {
+        id: 1,
+        name: 'Pendidikan',
+      },
+      status: 'PUBLISHED',
+    },
+    {
+      title: 'The Future of Public Services: Challenges and Opportunities',
+      updated_at: '06/02/2023',
+      category: {
+        id: 2,
+        name: 'Ekonomi',
+      },
+      status: 'PUBLISHED',
+    },
+    {
+      title: 'The Impact of Technology on Public Services Delivery',
+      updated_at: '06/02/2023',
+      category: {
+        id: 3,
+        name: 'Berita Umum',
+      },
+      status: 'DRAFT',
+    },
+    {
+      title:
+        'How Public-Private Partnerships are Revolutionizing Public Services',
+      updated_at: '06/02/2023',
+      category: {
+        id: 2,
+        name: 'Ekonomi',
+      },
+      status: 'DRAFT',
+    },
+    {
+      title:
+        'Maximizing Efficiency in Public Service Provision through Innovation',
+      updated_at: '06/02/2023',
+      category: {
+        id: 3,
+        name: 'Berita Umum',
+      },
+      status: 'ARCHIVED',
+    },
+  ]
 </script>
