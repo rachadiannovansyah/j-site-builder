06441b88868abd8dd583eddec9215bff943f1366
Yoslie <yoslie.tan@gmail.com>
2023-12-18 14:31:39 +0700

    refactor: refactor loading skeleton post list

--- components/Loading/ListSkeleton.vue
+++ components/Loading/ListSkeleton.vue
@@ -1,11 +1,4 @@
 <template>
-  <div class="mb-8 flex items-start px-6">
-    <div class="flex gap-8">
-      <div class="h-8 w-[144px] animate-pulse rounded-xl bg-gray-100"></div>
-      <div class="h-8 w-8 animate-pulse rounded-md bg-gray-100"></div>
-      <div class="h-8 w-[144px] animate-pulse rounded-xl bg-gray-100"></div>
-    </div>
-  </div>
   <ul role="list" class="flex flex-col gap-[30px] px-6">
     <li v-for="item in 6" :key="item" class="flex items-center gap-[30px]">
       <span class="h-8 w-8 animate-pulse rounded-md bg-gray-100"> </span>
