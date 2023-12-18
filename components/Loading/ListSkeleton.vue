c9813288cdf80b56a706f251f49a42a436d30248
Yoslie <yoslie.tan@gmail.com>
2023-12-18 10:05:12 +0700

    update: add loading list skeleton

---
+++ components/Loading/ListSkeleton.vue
@@ -0,0 +1,10 @@
+<template>
+  <ul role="list" class="flex flex-col gap-3">
+    <li v-for="item in 5" :key="item" class="flex items-center gap-[10px]">
+      <span class="h-11 w-8 rounded-lg bg-gray-200"> </span>
+      <div
+        class="h-11 w-full animate-pulse rounded-lg border bg-gray-200"
+      ></div>
+    </li>
+  </ul>
+</template>
