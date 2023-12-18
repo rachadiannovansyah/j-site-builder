2a76c020f23b8df3ba7dd175bfb1943fa0502f8d
Yoslie <yoslie.tan@gmail.com>
2023-12-18 10:45:51 +0700

    style: update loading skeleton list style

--- components/Loading/ListSkeleton.vue
+++ components/Loading/ListSkeleton.vue
@@ -1,10 +1,15 @@
 <template>
-  <ul role="list" class="flex flex-col gap-3">
-    <li v-for="item in 5" :key="item" class="flex items-center gap-[10px]">
-      <span class="h-11 w-8 rounded-lg bg-gray-200"> </span>
-      <div
-        class="h-11 w-full animate-pulse rounded-lg border bg-gray-200"
-      ></div>
+  <div class="mb-8 flex items-start px-6">
+    <div class="flex gap-8">
+      <div class="h-8 w-[144px] animate-pulse rounded-xl bg-gray-100"></div>
+      <div class="h-8 w-8 animate-pulse rounded-md bg-gray-100"></div>
+      <div class="h-8 w-[144px] animate-pulse rounded-xl bg-gray-100"></div>
+    </div>
+  </div>
+  <ul role="list" class="flex flex-col gap-[30px] px-6">
+    <li v-for="item in 6" :key="item" class="flex items-center gap-[30px]">
+      <span class="h-8 w-8 animate-pulse rounded-md bg-gray-100"> </span>
+      <div class="h-8 w-full animate-pulse rounded-xl bg-gray-100"></div>
     </li>
   </ul>
 </template>
