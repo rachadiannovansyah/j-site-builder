40b0037b82cc74ce4276f0f4a194be49ad08fba6
Ibwedagama <wardana.wedagama@gmail.com>
2024-01-08 09:47:31 +0800

    feat (respository): add getTags method

--- repository/j-site/modules/tag.ts
+++ repository/j-site/modules/tag.ts
@@ -1,10 +1,26 @@
 import { AsyncDataOptions } from '#app'
 
 import FetchFactory from '../../factory'
-import { ITagResponse, ITagRequestBody } from '../types/tag'
+import { ITagResponse, ITagsResponse, ITagRequestBody } from '../types/tag'
+import { FetchOptions } from 'ofetch'
 
 class TagModules extends FetchFactory {
   private RESOURCE = '/v1/tags'
+
+  async getTags(
+    settingId: string,
+    fetchOptions?: FetchOptions<'json'>,
+    options?: AsyncDataOptions<ITagsResponse>,
+  ) {
+    return useAsyncData(() => {
+      return this.call<ITagsResponse>(
+        'GET',
+        `${this.RESOURCE}/${settingId}`,
+        undefined,
+        fetchOptions,
+      )
+    }, options)
+  }
 
   async createTag(
     settingId: string,
