e789b832d3f273ceb8ebcb810ff085633ba9c94e
Ibwedagama <wardana.wedagama@gmail.com>
2024-01-10 12:14:42 +0800

    refactor (post): move form title to separate component

--- components/Post/Form.vue
+++ components/Post/Form.vue
@@ -13,24 +13,7 @@
     <section class="grid grid-cols-[1fr,364px] gap-2.5">
       <div class="grid grid-cols-1 gap-y-2.5">
         <div class="rounded-lg bg-white p-[14px]">
-          <p
-            class="mb-[14px] font-roboto text-base font-medium leading-6 text-green-700"
-          >
-            Judul
-          </p>
-          <UFormGroup name="title">
-            <UTextarea
-              v-model="title"
-              :rows="2"
-              placeholder="Masukkan judul berita"
-              maxlength="255"
-            />
-            <template #help>
-              <span class="font-lato text-xs text-gray-600">
-                Tersisa {{ 255 - title.length }} Karakter
-              </span>
-            </template>
-          </UFormGroup>
+          <PostFormTitle />
         </div>
 
         <div class="rounded-lg bg-white p-[14px]">
@@ -472,14 +455,6 @@
   const emit = defineEmits(['submit-form'])
 
   /* ------------------------- Post Store Data Binding ------------------------ */
-  const title = computed({
-    get() {
-      return postStore.form.title
-    },
-    set(value) {
-      postStore.setTitle(value)
-    },
-  })
 
   const content = computed({
     get() {
---
+++ components/Post/FormTitle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="rounded-lg bg-white p-[14px]">
+    <p
+      class="mb-[14px] font-roboto text-base font-medium leading-6 text-green-700"
+    >
+      Judul
+    </p>
+    <UFormGroup name="title">
+      <UTextarea
+        v-model="title"
+        :rows="2"
+        placeholder="Masukkan judul berita"
+        maxlength="255"
+      />
+      <template #help>
+        <span class="font-lato text-xs text-gray-600">
+          Tersisa {{ 255 - title.length }} Karakter
+        </span>
+      </template>
+    </UFormGroup>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const postStore = usePostStore()
+
+  const title = computed({
+    get() {
+      return postStore.form.title
+    },
+    set(value) {
+      postStore.setTitle(value)
+    },
+  })
+</script>
+
+<style lang="scss" scoped></style>
