63d351e81a06579de65b727cb056276403706863
Ibwedagama <wardana.wedagama@gmail.com>
2024-01-17 18:16:30 +0700

    feat (post): add data cy to form title component

--- components/Post/FormTitle.vue
+++ components/Post/FormTitle.vue
@@ -2,6 +2,7 @@
   <div class="rounded-lg bg-white p-[14px]">
     <p
       class="mb-[14px] font-roboto text-base font-medium leading-6 text-green-700"
+      data-cy="post-form__title-section__title"
     >
       Judul
     </p>
@@ -10,10 +11,14 @@
         v-model="title"
         :rows="2"
         placeholder="Masukkan judul post"
+        data-cy="post-form__title-section__input-text"
         maxlength="255"
       />
       <template #help>
-        <span class="font-lato text-xs text-gray-600">
+        <span
+          class="font-lato text-xs text-gray-600"
+          data-cy="post-form__title-section__remaining-chars"
+        >
           Tersisa {{ 255 - title.length }} Karakter
         </span>
       </template>
